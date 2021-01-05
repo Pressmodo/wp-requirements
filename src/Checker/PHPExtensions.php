770d9996299723683024ee02961469dc8dad47d4
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2021-01-05 16:24:52 +0000

    remove textdomain load for now

--- src/Checker/PHPExtensions.php
+++ src/Checker/PHPExtensions.php
@@ -52,7 +52,7 @@ class PHPExtensions extends Abstracts\Checker {
 		if ( ! empty( $missing_extensions ) ) {
 			$this->add_error( sprintf(
 				// Translators: PHP extensions.
-				_n( 'Missing PHP extension: %s', 'Missing PHP extensions: %s', count( $missing_extensions ), Requirements::$textdomain, 'wp-requirements' ),
+				'Missing PHP extension: %s',
 				implode( ', ', $missing_extensions )
 			) );
 		}
--- src/Requirements.php
+++ src/Requirements.php
@@ -84,8 +84,6 @@ class Requirements {
 			$this->load_default_checkers();
 		}
 
-		load_textdomain( self::$textdomain, dirname( __DIR__ ) . '/languages/wp-requirements-' . get_locale() . '.mo' );
-
 	}
 
 	/**
